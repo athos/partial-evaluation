cd6bcd48d9b2ab0b8a9ac67f535edcfca0c3bafd
OHTA Shogo <athos0220@gmail.com>
2014-04-29 16:50:54 +0900

    add some dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.match "0.2.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [com.cemerick/pomegranate "0.3.0"]]}})
