4cdb66320e6318729d3019580bb86e2e3a5c41b0
OHTA Shogo <athos0220@gmail.com>
2014-05-01 09:17:08 +0900

    add run function

--- src/partial_evaluation/flowchart-lang.clj
+++ src/partial_evaluation/flowchart-lang.clj
@@ -40,3 +40,14 @@
                (if-let [f (fs op)]
                  (apply f (map #(eval env %) args))))
         :else expr))
+
+(defn run [commands env program]
+  (let [[command & commands] commands]
+    (match command
+      ['goto l] (recur (program l) env program)
+      ['set! v e] (recur commands (update env v (eval env e)) program)
+      ['if test 'goto l1 'else l2]
+      #_=> (if (= (eval env test) 0)
+             (recur (program l1) env program)
+             (recur (program l2) env program))
+      ['return e] (eval env e))))
