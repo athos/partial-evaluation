bb2a126b71883fbaf058eecce90b6d6cfef4969c
OHTA Shogo <athos0220@gmail.com>
2014-05-02 07:38:31 +0900

    add dev/user.clj

---
+++ dev/user.clj
@@ -0,0 +1,15 @@
+(ns user
+  (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [partial-evaluation.flowchart-lang :as flow]))
+
+(def env
+  '{n 10, a 0, b 1, t 0})
+
+(def program
+  '{1 [[if (= n 0) goto 2 else 3]]
+    2 [[return a]]
+    3 [[set! t a]
+       [set! a b]
+       [set! b (+ b t)]
+       [set! n (- n 1)]
+       [goto 1]]})
