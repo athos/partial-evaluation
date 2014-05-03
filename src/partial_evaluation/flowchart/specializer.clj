9ee7ebac0abe972175547e5af5c933860f544cff
OHTA Shogo <athos0220@gmail.com>
2014-05-03 12:02:35 +0900

    take into account division and static env for specialization

--- src/partial_evaluation/flowchart/specializer.clj
+++ src/partial_evaluation/flowchart/specializer.clj
@@ -1,6 +1,11 @@
 (ns partial-evaluation.flowchart.specializer
-  (:require [clojure.core.match :refer [match]])
+  (:refer-clojure :exclude [eval])
+  (:require [clojure.core.match :refer [match]]
+            [partial-evaluation.flowchart.interpreter :refer [eval]])
   (:import clojure.lang.PersistentQueue))
+
+(defn static? [division var]
+  (= (division var) :static))
 
 (defn specialize [division senv program]
   (loop [[[pp senv :as state] :as pending] (conj PersistentQueue/EMPTY [1 senv]),
@@ -11,17 +16,26 @@
       (let [visited (conj visited state)
             specialize-block
             (fn [block]
-              (loop [[command & commands] block, code [], pending (pop pending)]
+              (loop [[command & commands] block,
+                     senv senv,
+                     code [],
+                     pending (pop pending)]
                 (if (nil? command)
                   [code pending]
                   (match command
                     ['set! v e]
-                    #_=> (recur commands (conj code command) pending)
+                    #_=> (if (static? division v)
+                           (recur commands (assoc senv v (eval senv e)) code pending)
+                           (recur commands senv (conj code command) pending))
                     ['goto l]
-                    #_=> (recur (program l) code pending)
+                    #_=> (recur (program l) senv code pending)
                     ['if test 'goto l1 'else l2]
-                    #_=> [(conj code command)
-                          (into pending (remove visited [[l1 senv] [l2 senv]]))]
+                    #_=> (if (static? division test) ; FIXME: test may be an arbitrary expression
+                           (if (= (eval senv test) 1)
+                             (recur (program l1) senv code pending)
+                             (recur (program l2) senv code pending))
+                           [(conj code command)
+                            (into pending (remove visited [[l1 senv] [l2 senv]]))])
                     ['return e]
                     #_=> [(conj code command) pending]))))
             [code pending] (specialize-block (program pp))]
