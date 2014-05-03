e2bc6a43a190af3623aab7edbca21f7e1c5c470a
OHTA Shogo <athos0220@gmail.com>
2014-05-03 12:20:14 +0900

    now simple constant folding works

--- src/partial_evaluation/flowchart/specializer.clj
+++ src/partial_evaluation/flowchart/specializer.clj
@@ -1,11 +1,24 @@
 (ns partial-evaluation.flowchart.specializer
   (:refer-clojure :exclude [eval])
   (:require [clojure.core.match :refer [match]]
-            [partial-evaluation.flowchart.interpreter :refer [eval]])
+            [partial-evaluation.flowchart.interpreter :refer [eval] :as interp])
   (:import clojure.lang.PersistentQueue))
 
 (defn static? [division var]
   (= (division var) :static))
+
+(defn constant? [exp]
+  (number? exp))
+
+(defn fold [senv exp]
+  (cond (symbol? exp) (or (senv exp) exp)
+        (coll? exp)
+        #_=> (let [[op & args] exp
+                   args' (map #(fold senv %) args)]
+               (if (every? constant? args')
+                 (apply (interp/primitive-implementation op) args')
+                 (cons op args')))
+        :else exp))
 
 (defn specialize [division senv program]
   (loop [[[pp senv :as state] :as pending] (conj PersistentQueue/EMPTY [1 senv]),
@@ -26,7 +39,7 @@
                     ['set! v e]
                     #_=> (if (static? division v)
                            (recur commands (assoc senv v (eval senv e)) code pending)
-                           (recur commands senv (conj code command) pending))
+                           (recur commands senv (conj code ['set! v (fold senv e)]) pending))
                     ['goto l]
                     #_=> (recur (program l) senv code pending)
                     ['if test 'goto l1 'else l2]
@@ -37,6 +50,6 @@
                            [(conj code command)
                             (into pending (remove visited [[l1 senv] [l2 senv]]))])
                     ['return e]
-                    #_=> [(conj code command) pending]))))
+                    #_=> [(conj code ['return (fold senv e)]) pending]))))
             [code pending] (specialize-block (program pp))]
         (recur pending visited (assoc residual state code))))))
